e33620530bd3090c8fe4d135136e58c5449f4de7
Peter Hosey <prh@Four-Sided-Cheese-Grater.local>
2011-12-22 00:49:40 -0800

    Moved -testDeprecatedFunctions to a new test case class, and added a pair of private classes in its module to test the implemented-deprecated-method warning.

--- WarningsTest/WarningsTestTests/AssortedWarningTests.m
+++ WarningsTest/WarningsTestTests/AssortedWarningTests.m
@@ -79,18 +79,6 @@
 	}
 }
 
-- (void) testDeprecatedFunctions {
-	NSMutableArray *arrayOfOneThing = [NSMutableArray arrayWithObject:@"Frank"];
-	enum { numIndices = 1U };
-	NSUInteger indices[numIndices] = { 0UL };
-	STAssertNoThrow([arrayOfOneThing removeObjectsFromIndices:indices numIndices:numIndices], @"Removing an object threw an exception!");
-
-	NSWindow *window = nil;
-	if (window || (time(NULL) == 0)) {
-		NSRunAlertPanelRelativeToWindow(NSLocalizedString(@"OH NO!", /*comment*/ NULL), NSLocalizedString(@"The sky is falling!", /*comment*/ NULL), /*defaultButton*/ NSLocalizedString(@"Run!", /*comment*/ NULL), /*alternateButton*/ nil, /*otherButton*/ nil, window);
-	}
-}
-
 - (void) testOverloadedVirtualFunctions {
 	//We can't actually test this in pure Objective-C code. Someone will need to write an Objective-C++ test case class to do it.
 }
---
+++ WarningsTest/WarningsTestTests/DeprecationWarningTests.h
@@ -0,0 +1,12 @@
+//
+//  DeprecationWarningTests.h
+//  WarningsTest
+//
+//  Created by Peter Hosey on 2011-12-22.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface DeprecationWarningTests : SenTestCase
+
+@end
