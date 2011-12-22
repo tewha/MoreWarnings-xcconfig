078db5dd2be4a8b9340f8435b6ae21d12be32726
Peter Hosey <prh@Four-Sided-Cheese-Grater.local>
2011-12-21 20:48:50 -0800

    Added a project containing a unit test bundle. No actual tests yet.

---
+++ WarningsTest/WarningsTestTests/WarningsTestTests.h
@@ -0,0 +1,12 @@
+//
+//  WarningsTestTests.h
+//  WarningsTestTests
+//
+//  Created by Peter Hosey on 2011-12-21.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface WarningsTestTests : SenTestCase
+
+@end
---
+++ WarningsTest/WarningsTestTests/WarningsTestTests.m
@@ -0,0 +1,17 @@
+//
+//  WarningsTestTests.m
+//  WarningsTestTests
+//
+//  Created by Peter Hosey on 2011-12-21.
+//
+
+#import "WarningsTestTests.h"
+
+@implementation WarningsTestTests
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in WarningsTestTests");
+}
+
+@end
